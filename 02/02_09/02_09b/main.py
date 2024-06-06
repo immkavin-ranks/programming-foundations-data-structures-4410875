182091a7284e6ca2a78ab46594b26318fc1a92be
Kavin <immkavin.ofcl@gmail.com>
2024-06-06 07:22:39 +0000

    Find second smallest

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,8 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) > 1:
+        sorted_list = sorted(my_list)
+        return sorted_list[1]
+    return None
+    
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,17 @@
-def find_second_smallest(my_list):
-    return 0
+# O(n)
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+    smallest = float("inf")
+    second_smallest = float("inf")
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+
+    return second_smallest
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
